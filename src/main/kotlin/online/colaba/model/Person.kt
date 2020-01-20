52bb2e20881b7d32558b16d5e1a4df6961837337
Dmitry Koltovich <steklopod@gmail.com>
2020-01-20 21:28:33 +0300

    Refactoring model of Person both in one file

--- src/main/kotlin/online/colaba/model/Person.kt
+++ src/main/kotlin/online/colaba/model/Person.kt
@@ -11,5 +11,13 @@ data class Person(
     constructor() : this(null, null, null, null)
 }
 
+data class PersonDto(
+    var firstName: String?,
+    var lastName: String?,
+    var phone: String?,
+    var birthdate: LocalDate?
+) {
+    constructor() : this(null, null, null, null)
+}
 
 
